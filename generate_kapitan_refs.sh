e83d786805cf3833423719a0fffb67ba7e0986c2
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-05-16 17:00:01 +0300

    Slow Kapitan write

---
+++ generate_kapitan_refs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# docker run -t --rm -v $(pwd):/src:delegated --entrypoint "/bin/bash" kapicorp/kapitan:v0.32.0-rc.0 "generate_kapitan_refs.sh"
+
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+echo "here1"
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+echo "here2"
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+echo "here3"
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
+time (echo "secret_value!" | kapitan refs --write plain:test_secret -f -)
